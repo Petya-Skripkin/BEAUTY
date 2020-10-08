8e4e07f8f7c72c8d1288d962b9639de45bbc18b7
Petya-Skripkin <sk.petya02@mail.ru>
2020-10-08 13:09:31 +0600

    First settings

---
+++ src/components/header/header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Content } from './styles';
+
+const Header = () => (
+  <Content>
+    Hello
+  </Content>
+)
+
+export default Header
---
+++ src/pages/demo/demo.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import * as Components from '../../components';
+
+const Demo = () => (
+  <Components.Header />
+)
+
+export default Demo;
